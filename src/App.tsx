de45253537e3f043deed401ed11b95bba5a5a976
Destin <d.gandobak@gmail.com>
2020-05-10 14:28:36 +0200

    Affichage améliorer avec Materialize css

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,30 @@ useEffect( () => {
 	setPokemons(POKEMONS);
 }, []);
 
- return (
- <div>
-     <h1>Pokédex</h1>
-     <p>Il y a {pokemons.length} pokémons dans le Pokédex</p>
- </div>
- )
+	return (
+		<div>
+			<h1 className="center">Pokédex</h1>
+			<div className="container">
+				<div className="row">
+					{pokemons.map( ({id, name, picture, created}) => (
+						<div className="col s6 m4" key={id}>
+							<div className="card horizontal">
+								<div className="card-image">
+									<img src={picture} alt={name}/>
+								</div>
+								<div className="card-stacked">
+									<div className="card-content">
+										<p>{name}</p>
+										<p>{created.toString()}</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					))}
+				</div>
+			</div>
+		</div>
+	);
 }
   
 export default App;
